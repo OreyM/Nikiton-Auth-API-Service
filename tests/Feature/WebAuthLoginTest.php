ab2c8a4a833454ab87bc70e8ba141852aa38369c
OreyM <oreymgt@gmail.com>
2025-08-03 22:04:52 +0200

    WebAuthLoginTest

---
+++ tests/Feature/WebAuthLoginTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class WebAuthLoginTest extends TestCase
+{
+    public function test_web_auth_login_returns_a_method_not_allowed_response(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(405);
+    }
+}
