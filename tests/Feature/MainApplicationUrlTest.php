376c712291ecf15a9881ce365a1bbb48a66cf1dd
OreyM <oreymgt@gmail.com>
2025-08-03 22:02:00 +0200

    MainApplicationUrlTest

---
+++ tests/Feature/MainApplicationUrlTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class MainApplicationUrlTest extends TestCase
+{
+    public function test_the_application_returns_a_method_not_allowed_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(405);
+    }
+}
